7ca8cc8120a01975f0a0ed2b905a2e16363f7f30
Muthu <s.muthukhumar@gmail.com>
2019-09-23 16:07:20 +0800

    Missing Permission added

--- app/src/main/java/com/app/videocapture/MainActivity.kt
+++ app/src/main/java/com/app/videocapture/MainActivity.kt
@@ -2,17 +2,20 @@ package com.app.videocapture
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import android.net.Uri
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
@@ -32,24 +35,37 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             getPermissionToRecordVideo()
-            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
-            intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 3)
-            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
-            // intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri) // set the image file
-            startActivityForResult(intent, CAMERA_CAPTURE_VIDEO_REQUEST_CODE)
+
         }
+    }
+
+
+    private fun captureVideoFor3Seconds() {
+        val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 3)
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
+        startActivityForResult(intent, CAMERA_CAPTURE_VIDEO_REQUEST_CODE)
     }
 
 
     private fun getPermissionToRecordVideo() {
         Dexter.withActivity(this)
-            .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            .withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.RECORD_AUDIO,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA)
-            .withListener(object :MultiplePermissionsListener{
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report?.areAllPermissionsGranted()!!) {
+                        captureVideoFor3Seconds()
+                        return
+                    }
 
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        showSettingsDialog()
+                    }
 
                 }
 
@@ -58,18 +74,18 @@ class MainActivity : AppCompatActivity() {
                     token: PermissionToken?
                 ) {
 
-
+                    token?.continuePermissionRequest()
                 }
 
 
-            })
+            }).check()
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode === Activity.RESULT_OK) {
+        if (resultCode === Activity.RESULT_OK && requestCode == CAMERA_CAPTURE_VIDEO_REQUEST_CODE) {
             val vid = data?.data
             videoPath = getRealPathFromURI(vid!!)
             vv.setVideoURI(Uri.parse(videoPath))
@@ -82,5 +98,27 @@ class MainActivity : AppCompatActivity() {
         return FilePath.getPath(this, contentUri)
     }
 
+    private fun showSettingsDialog() {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Need Permissions")
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
+        builder.setPositiveButton("GOTO SETTINGS") { dialog, _ ->
+            dialog.cancel()
+            openSettings()
+        }
+        builder.setNegativeButton(
+            "Cancel"
+        ) { dialog, _ -> dialog.cancel() }
+        builder.show()
+
+    }
+
+    // navigating user to app settings
+    private fun openSettings() {
+        val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivityForResult(intent, 101)
+    }
 
 }
